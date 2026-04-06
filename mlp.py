7958dc3b75a794da50d6f906b3789ee632d685b4
Mohammed <mohammedhdf03@gmail.com>
2026-04-06 21:36:23 +0100

    v0.1

--- mlp.py
+++ mlp.py
@@ -1,14 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print("MLP from scratch")
-a = np.linspace(1,10,9)
-b=  np.array([[1,2,3],[2,3,3]])
-d = b.reshape(b.size)
-d = d.reshape(d.size,1)
-c = d.shape
-print(c)
-print(d)
 def f(x,y):
     return np.sin(np.sqrt(x**2+y**2))+0.5*np.cos(2*x+2*y)
 def CalculerAltitude(m):
@@ -21,7 +13,8 @@ data = rng.uniform(-5,5,(2000,2))
 print(data)
 print(data.shape)
 print (f(2,3))
-l = CalculerAltitude(data)
-print(l)
-print(l.shape)
-print(l.size)
+Z = CalculerAltitude(data)
+print(Z)
+print(Z.shape)
+print(Z.size)
+print("DD")
